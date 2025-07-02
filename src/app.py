f8c12f55bc1be766f423aa3cca40d9a9c562538e
Tunde Oladejo <oladejotunde60@gmail.com>
2025-07-02 03:10:14 +0100

    self-hosted

--- src/app.py
+++ src/app.py
@@ -16,7 +16,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message': 'This is a simple Flask API for demonstration purposes-15.'
+        'message': 'This is a simple Flask API for demonstration purposes-16.'
     })
 
 @app.route('/api/v1/healthz')
